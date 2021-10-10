84226c34f44e45ad476159d1ee9d0087b5c3e847
drypot <drypot@gmail.com>
2021-10-11 01:05:34 +0900

    try rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+//import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript'
+import { defineConfig } from 'rollup'
+
+export default defineConfig({
+  input: 'src/ts/main.ts',
+  output: {
+    file: 'public/build/bundle.js',
+    format: 'iife'
+  },
+  watch: {
+    include: [
+      "src/ts/**/*.ts"
+    ]
+  },
+  plugins: [
+    typescript({
+      //include: "src/ts/**/*.ts",
+      cacheDir: 'tmp/rollup-tscache',
+    }),
+    //resolve(),
+  ]
+})
